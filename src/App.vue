da28455b56bbcca7e560078653e208e3ee84afbc
PSJ-F8O1999\PSJ F8O1999 <614234007@parichat.skru.ac.th>
2021-01-21 09:04:54 +0700

    Update Directive - 007

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
+    {{ counter }}<br/>
+    <button v-on:click="counter=counter+1">Click1</button><br/>
+    <button v-on:click="clicked">Click2</button>
+    <!--v-on:click ตัวย่อคือ @click-->
+    <hr />
     <h1>{{ message }}</h1>
     <br />
     <span v-text="message"></span>
     <hr />
 
+    <input type="text" v-model="message" />
+    <hr />
     <h1 v-show="isShow">Hello!</h1>
 
     <hr />
@@ -17,19 +24,34 @@
 
     <hr />
     <div v-bind:key="emp.id" v-for="emp in employees">
-      <span v-bind:class="(emp.SALARY>90000) ? 'high' : (emp.SALARY>=30000) ? 'medium' :'low' ">
+      <span
+        v-bind:class="
+          emp.SALARY > 90000 ? 'high' : emp.SALARY >= 30000 ? 'medium' : 'low'
+        "
+      >
         {{ emp.NAME }}
       </span>
 
       <span v-if="emp.SALARY > 90000">⭐⭐⭐</span>
       <span v-else-if="emp.SALARY >= 30000">⭐⭐</span>
       <span v-else>⭐</span>
-
-      <hr />
     </div>
     <a v-bind:href="urlYT">Youtube</a>
     <hr />
-    <img v-bind:src="require('@/assets/images/'+ picture + '.png')" width="500px"/>
+    <img
+      v-bind:src="require('@/assets/images/' + picture + '.png')"
+      width="500px"
+    />
+<hr/>
+<select v-model="message">
+<option value="Bread">Bread</option>
+<option value="Candy">Candy</option>
+<option value="Coke">Coke</option>
+
+</select>
+<hr/>
+<textarea v-model="message"></textarea>
+
   </div>
 </template>
 
@@ -39,8 +61,9 @@ export default {
   components: {},
   data() {
     return {
-      picture:"a",
-      urlYT:"https://www.youtube.com/?gl=TH",
+      counter:0,
+      picture: "a",
+      urlYT: "https://www.youtube.com/?gl=TH",
       message: "สวัสดี",
       isShow: true,
       employees: [
@@ -50,6 +73,11 @@ export default {
       ],
     };
   },
+  methods:{
+      clicked: function(){
+        this.counter=this.counter+2;
+      }
+    }
 };
 </script>
 
